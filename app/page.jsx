5f01343a5a58ab95b3d7bddc375a7d7635909c2b
arosha619 <sandaruwanmdgar.19@itfac.mrt.ac.lk>
2025-06-28 23:33:08 +0530

    responsive home

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import { FaInstagram, FaFacebook, FaLinkedin } from 'react-icons/fa';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col lg:flex-row items-center justify-between px-4 py-12 bg-white dark:bg-gray-900 gap-8 lg:gap-4 pb-24">
+    <div className="min-h-screen flex flex-col lg:flex-row items-center max-w-screen-xl mx-auto px-4 py-12 gap-8 lg:gap-4 pb-24">
       {/* Left: Content */}
-      <div className="flex-1 flex flex-col justify-center items-center lg:items-start text-center lg:text-left order-2 lg:order-1 lg:pl-32">
+      <div className="flex-1 flex flex-col justify-center items-center lg:items-start text-center lg:text-left order-2 lg:order-1 lg:pl-32 lg:mt-20">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-gray-900 dark:text-white mb-2">
           Hello,<br />I am <span className="text-blue-600 dark:text-blue-400">Yasas Sri Wickramasinghe</span>
         </h1>
